8a91f6b61f24301a0456d6a50bc39c512864449f
Egor Elchinov <elchinov.es@phystech.edu>
2020-03-19 16:58:08 +0300

    created asm_dfa source files

---
+++ Aho/asm_dfa.h
@@ -0,0 +1,24 @@
+#ifndef AHO_ASM_DFA_H
+#define AHO_ASM_DFA_H
+
+#include "aho_corasick.h"
+
+struct SAsmDfa
+{
+    uint32_t state_cnt;
+    uint32_t* link_arr;
+    uint32_t* edge_map[AHO_SYMB_CNT];
+};
+
+int init_asm_dfa(struct SAsmDfa* asm_dfa, const struct SAhoTree* aho_tree);
+int delete_asm_dfa(struct SAsmDfa* asm_dfa);
+
+int dump_asm_dfa(const struct SAsmDfa* asm_dfa, 
+                 FILE* fout);
+
+int dump_asm_dfa_state(const struct SAsmDfa* asm_dfa, uint32_t state_idx, 
+                       FILE* fout);
+
+int testSAsmDfa();
+
+#endif //AHO_ASM_DFA_H
